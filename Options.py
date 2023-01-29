dc46dee55bf848f4fe54ef6179471ba810d42fab
Guhan Gnanam <116681361+guhangnanam@users.noreply.github.com>
2023-01-28 23:53:56 -0500

    Update Options.py

--- Options.py
+++ Options.py
@@ -1,3 +1,5 @@
+import main
+
 space = "    "
 import pyfiglet
 import time
@@ -6,6 +8,8 @@ from Monsters import *
 from Timer import *
 # from main import *
 global future
+global x
+x = False
 from Timer import *
 
 # sevenCount = 0
@@ -32,13 +36,13 @@ def oneOption():
     if option == "2":
         print(pyfiglet.figlet_format("HELP", font = "slant", justify = "center", width = 110))
         time.sleep(wait)
-        print("Nobody hears you")
+        print("Your voice echoes")
         time.sleep(wait)
-        print("Towering mountains lean over you")
+        print("A loud growl comes from inside the cave")
         time.sleep(wait)
-        print("The ground under you torn boots is covered in thick grass")
-        time.sleep(wait)
-        print("In front of you is a cave")
+        oneOption()
+
+
 
 
 def twoOption():
@@ -53,11 +57,11 @@ def twoOption():
         time.sleep(wait)
         print("The air is heavy, almost like it's pushing you down, or keeping you away")
         time.sleep(wait)
-        print(pyfiglet.figlet_format("plop"))
+        print(pyfiglet.figlet_format("plop", justify="center"))
         time.sleep(wait)
         print("Somewhere down the cave, you can hear water dripping")
         time.sleep(wait)
-        print(pyfiglet.figlet_format("plop"))
+        print(pyfiglet.figlet_format("plop", justify="center"))
         time.sleep(wait)
         print("You follow the sound")
 
@@ -68,23 +72,80 @@ def twoOption():
         time.sleep(wait)
         print("As you prepare to climb the mountain, you remember your equipment")
         time.sleep(wait)
-        print("Gloves, a knife, and some matches")
+        print("Gloves, a knife, and a lighter")
         time.sleep(wait)
         print("You look up at the towering mountains, unsure if you could make it to the top")
         time.sleep(wait)
         print("You have second doubts")
-        twoOption()
+        time.sleep(wait)
+        print("Are you sure you want to climb the mountain? Y/N")
+        option = input()
+        if option == "Y":
+            time.sleep(wait)
+            print("You grit your teeth and climb the mountain")
+            time.sleep(wait)
+            print("You near the top but you are unable to maintain your grip")
+            time.sleep(wait)
+            print("YOU HAVE FALLEN TO YOUR DEATH")
+            return True
+
+        else:
+            print("You reconsider your options")
+            twoOption()
 
 
 
+def clueOption():
+    light = False
+    print("You come across markings on the wall")
+    time.sleep(wait)
+    print("1. Inspect Markings", end= space)
+    print("2. Check supplies", end= space)
+    print("3. Continue forward")
+    option = input()
+    time.sleep(wait)
 
+    if option == "2":
+        print("You check inside your bag")
+        time.sleep(wait)
+        print("You have a knife, a gloves, and a lighter")
+        time.sleep(wait)
+        print("You take the lighter out")
+        light = True
+        print("1. Inspect Markings", end=space)
+        print("2. Check supplies", end=space)
+        print("3. Continue forward")
+        option = input()
+
+
+    if option == "1" and light == True:
+        # dragon correct path is RRL
+        # cow correct path is LRLLR
+        print(pyfiglet.figlet_format("The key to your survival is hidden in this message", font="slant", justify="center", width=100))
+        time.sleep(wait)
+        print(pyfiglet.figlet_format("if you face the fiery beast", font="slant", justify="center", width=100))
+        time.sleep(wait)
+        print(pyfiglet.figlet_format("and do not desire to become its feast", font="slant", justify="center", width=100))
+        time.sleep(wait)
+        print(pyfiglet.figlet_format("Turn to the right three times, follow my behest", font="slant", justify="center", width=100))
+        time.sleep(wait)
+
+
+    elif option == "1":
+        print("It is too dark to read the symbols")
+        clueOption()
+
+    if option == "3":
+        pass
 
 def threeOption():
+    print("You proceed deeper into the cave")
+    print("You come to a cross roads")
     print("1. Turn Left", end=space)
     print("2. Turn Right")
     option = input()
 
-    if option == "1":
+    if option == "1" or option == "2":
         print(pyfiglet.figlet_format("ROOOAAARRR",justify= "center",width= 110))
         time.sleep(.5)
         printmainMonster()
@@ -96,21 +157,6 @@ def threeOption():
         print("RUN QUICK")
         fourOption()
 
-
-
-
-
-    if option == "2":
-        print(pyfiglet.figlet_format("MOOOOOOO", justify="center", width=110))
-        time.sleep(.5)
-        printOtherMonster()
-        time.sleep(wait)
-        print("The beast of the unknown is hunting you")
-        time.sleep(wait)
-        print("Escape the cave")
-        time.sleep(wait)
-        print("RUN QUICK")
-        fiveOption()
 
 def fourOption():
     print("1. Turn Left", end=space)
@@ -164,11 +210,11 @@ def eightOption():
 
 def nineOption():
     print("YOU RAN INTO A DEAD END")
-
+    return True
 
 def tenOption():
     print("YOU ESCAPED THE CAVE")
-
+    return True
 
 def elevenOption():
     print("1. Turn Left", end=space)
@@ -233,6 +279,3 @@ def seventeenOption():
 
 def eighteenOption():
     print("YOU ESCAPED THE CAVE")
-
-
-
